0e629216318f9a68132ced00cb92f9fb88952154
Vishva <adharsh.scipy@gmail.com>
2025-01-06 14:01:07 +0530

    node error

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./landing/LandingPage.jsx"
+import AdminLanding from "./components/admin/AdminLanding.jsx";
+import { Layout } from "antd"
+
+
+const { Sider } = Layout;
 
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <Sider className="sidebar">Sidebar</Sider>
+      </Layout>
       <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<LandingPage/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+
+          {/* admin */}
+
+          <Route path="/admin" element={<AdminLanding />} />
+
+        </Routes>
       </BrowserRouter>
     </div>
   );

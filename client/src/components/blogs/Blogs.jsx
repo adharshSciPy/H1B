5b73f65f489899dfdfa8745365e7d49dca7263e1
Vishva <adharsh.scipy@gmail.com>
2025-01-07 13:53:15 +0530

    sidebar pages

---
+++ client/src/components/blogs/Blogs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Blogs.css"
+import Sidebar from '../sidebar/Sidebar'
+
+function Blogs() {
+  return (
+    <div className='container'>
+      {/* <Sidebar /> */}
+      <div className="blogs-content">
+        <h1>Blogs</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Blogs
---
+++ client/src/components/socialmedia/Socialmedia.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Socialmedia.css"
+import Sidebar from '../sidebar/Sidebar'
+
+function Socialmedia() {
+  return (
+    <div className='container'>
+    {/* <Sidebar /> */}
+    <div className="social-content">
+      <h1>Social Media</h1>
+    </div>
+  </div>
+  )
+}
+
+export default Socialmedia

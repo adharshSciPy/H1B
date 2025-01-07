8fd01c44f66e381908a0650ef564c9832354aff0
Vishva <adharsh.scipy@gmail.com>
2025-01-07 11:49:04 +0530

    sidebar complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./landing/LandingPage.jsx"
 import AdminLanding from "./components/admin/AdminLanding.jsx";
-import { Layout } from "antd"
-
-
-const { Header, Sider } = Layout;
 
 function App() {
   return (
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,45 +1,74 @@
-import React from 'react'
+import { React, useState } from 'react'
 import Logo from "../../assets/header-logo.png"
 import "./Sidebar.css"
-import { Menu } from 'antd'
-import { HomeOutlined, UserOutlined, ContainerOutlined, DiffOutlined, UserAddOutlined, SettingOutlined, LogoutOutlined } from "@ant-design/icons"
+import { Menu, Button } from 'antd'
+import {
+    HomeOutlined, UserOutlined, ContainerOutlined, DiffOutlined, UserAddOutlined, SettingOutlined, LogoutOutlined, PushpinOutlined,
+    PushpinFilled,
+} from "@ant-design/icons"
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <div className='logo'>
-                <img src={Logo} />
-            </div>
-            <Menu>
-                <Menu.Item key='home' icon={<HomeOutlined />}>
-                    Dashboard
-                    <span></span>
-                </Menu.Item>
-                <Menu.Item key='home' icon={<UserOutlined />}>
-                    Profile
-                    <span></span>
-                </Menu.Item>
-                <Menu.Item key='home' icon={<ContainerOutlined />}>
-                    Listing
-                    <span></span>
-                </Menu.Item>
-                <Menu.Item key='home' icon={<DiffOutlined />}>
-                    Blogs
-                    <span></span>
-                </Menu.Item>
-                <Menu.Item key='home' icon={<UserAddOutlined />}>
-                    Social Media
-                    <span></span>
-                </Menu.Item><Menu.Item key='home' icon={<SettingOutlined />}>
-                    Settings
-                    <span></span>
-                </Menu.Item><Menu.Item key='home' icon={<LogoutOutlined />}>
-                    Signout
-                    <span></span>
-                </Menu.Item>
-            </Menu>
+    const [selectedKey, setSelectedKey] = useState("dashboard");
+    const [isPinned, setIsPinned] = useState(false);
+    const togglePin = () => {
+        setIsPinned((prev) => !prev);
+    };
 
-        </div>
+    const handleMenuClick = (e) => {
+        setSelectedKey(e.key);
+    };
+    return (
+        <>
+            <div className="container">
+                <div className={`sidebar ${isPinned ? "pinned" : "collapsed"}`}>
+                    <div className="sidebar-content">
+                        <Menu className='menu'
+                            mode="vertical"
+                            selectedKeys={[selectedKey]}
+                            onClick={handleMenuClick}
+                        >
+                            <div className='logo'>
+                                <img src={Logo} />
+                            </div>
+                            <Menu.Item key='dashboard' icon={<HomeOutlined />}>
+                                {isPinned && "Dashboard"}
+                                <span></span>
+                            </Menu.Item>
+                            <Menu.Item key='profile' icon={<UserOutlined />}>
+                                {isPinned && "Profile"}
+                                <span></span>
+                            </Menu.Item>
+                            <Menu.Item key='listing' icon={<ContainerOutlined />}>
+                                {isPinned && "Listing"}
+                                <span></span>
+                            </Menu.Item>
+                            <Menu.Item key='blogs' icon={<DiffOutlined />}>
+                                {isPinned && "Blogs"}
+                                <span></span>
+                            </Menu.Item>
+                            <Menu.Item key='socialmedia' icon={<UserAddOutlined />}>
+                                {isPinned && "Social Media"}
+                                <span></span>
+                            </Menu.Item><Menu.Item key='settings' icon={<SettingOutlined />}>
+                                {isPinned && "Settings"}
+                                <span></span>
+                            </Menu.Item><Menu.Item key='signout' icon={<LogoutOutlined />}>
+                                {isPinned && "Signout"}
+                                <span></span>
+                            </Menu.Item>
+                        </Menu>
+                    </div>
+                </div>
+                <div className="pin-pin">
+                    <Button
+                        className="pin-button"
+                        type="text"
+                        icon={isPinned ? <PushpinFilled /> : <PushpinOutlined />}
+                        onClick={togglePin}
+                    />
+                </div>
+            </div>
+        </>
     )
 }
 

deaba4b43ebd8ce81c082c73caa7a6535dac3fa2
gokul <gokulsunil2015@gmail.com>
2025-01-07 16:42:48 +0530

    header component

--- client/src/components/settings/Settings.jsx
+++ client/src/components/settings/Settings.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import "./Settings.css"
-import Sidebar from '../sidebar/Sidebar'
+import React from 'react';
+import "./Settings.css";
+import Sidebar from '../sidebar/Sidebar';
+// import { SearchOutlined } from '@ant-design/icons';
+import Header from '../header/Header';
 
 function Settings() {
   return (
-    <div className='container'>
+    <div className='settingContainer'>
       <Sidebar />
       <div className="settings-content">
-        <h1>Settings</h1>
+        {/* <div className="settings-searchBar">
+          <SearchOutlined className="search-icon" />
+          <input 
+            type="text" 
+            placeholder="Search here..." 
+            className="search-input"
+          />
+        </div> */}
+        <Header/>
+        <h1 className='settings-mainHeading'>Settings</h1>
       </div>
     </div>
-  )
+  );
 }
 
-export default Settings
+export default Settings;

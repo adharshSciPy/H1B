27d716ef522442144f4016615fca4ce843ac2206
gokul <gokulsunil2015@gmail.com>
2025-01-13 17:02:08 +0530

    profile integration

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import { registerAdmin,adminLogout,editAdmin, getCollaborators, getSingleCollaborator, deleteCollaborator} from '../controller/adminController.js';
+import { registerAdmin,adminLogout,editAdmin, getCollaborators, getSingleCollaborator, deleteCollaborator, viewAdmin} from '../controller/adminController.js';
 import upload from '../multer/multer.js';
 const adminRouter=Router();
 adminRouter.route('/registeradmin').post(registerAdmin);
 adminRouter.route('/logout').post(adminLogout);
+adminRouter.route('/viewadmin').get(viewAdmin);
+
 adminRouter.route('/getallcollaborators').get(getCollaborators);
 adminRouter.route('/getsinglecollaborator').get(getSingleCollaborator);
 adminRouter.route('/deletecollaborator').delete(deleteCollaborator);

584adb84e1fdb8cf0a3db45434dac58d7142a967
Sanju tl <sanjutl19@gmail.com>
2025-05-28 22:45:37 +0530

    blog

--- server/routes/blogRouter.js
+++ server/routes/blogRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createblog,deleteBlog } from "../controller/blogController.js";
+import { createblog,deleteBlog,listallBlog } from "../controller/blogController.js";
 import upload from '../multer/multer.js';
 
 
 const blogrouter = express.Router();
 blogrouter.post('/createblog', upload.single('image'), createblog);
+blogrouter.get('/listallblog',listallBlog);
 blogrouter.delete("/deleteblog/:id", deleteBlog);
 
 export default blogrouter;
